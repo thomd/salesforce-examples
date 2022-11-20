c13a544845ea743c115a55fedd4e031fe89c8f9c
Thomas Dürr <thomduerr@gmail.com>
2022-11-20 16:13:43 +0100

    objects

---
+++ show-all-objects-and-fields/force-app/main/default/aura/FetchObjects/FetchObjectsController.js
@@ -0,0 +1,8 @@
+({
+init : function(component, event, helper) {
+helper.getData(component, event, helper);
+},
+handleChange : function(component, event, helper){
+helper.getAllFields(component, event, helper);
+}
+})
